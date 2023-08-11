b8a91a536f12fe3085e89d055315cdf994ecfd32
mphatsoz <mphatsoz@gmail.com>
2023-08-11 05:20:21 -0700

    lets

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,11 @@
-# add_0.py
-def add(a, b):
-    return a + b
+#!/usr/bin/python3
+
+import add_0
+
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    a = 1
+    b = 2
+    sum_result = add_0.add(a, b)
+    print(f"{a} + {b} = {sum_result}")
 
