a3abcf0d62701d2c2f906726dd120b08c3984e5c
mphatsoz <mphatsoz@gmail.com>
2024-01-10 11:02:57 -0800

    paste

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,21 +1,8 @@
-import marshal
-import types
-
-def print_module_names():
-    with open("hidden_4.pyc", "rb") as file:
-        magic = file.read(4)
-        timestamp = file.read(4)
-        code_object = marshal.load(file)
-
-        if code_object.co_flags & 0x08:  # Check if it's a module
-            module = types.ModuleType("hidden_4")
-            exec(code_object, module.__dict__)
-            
-            names = [name for name in dir(module) if not name.startswith('__')]
-            names.sort()
-
-            for name in names:
-                print(name)
-
+#!/usr/bin/python3
 if __name__ == "__main__":
-    print_module_names()
+    import hidden_4
+    # Print sorted name from directory
+    for name in sorted(dir(hidden_4)):
+        # print only names that do not start with __
+        if name[:2] != '__':
+            print("{}".format(name))
